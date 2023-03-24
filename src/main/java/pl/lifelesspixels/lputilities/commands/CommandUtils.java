faffd3c9920b32149775814c671bd9f14cd2a89c
Karol Małecki <malecki.karol@outlook.com>
2023-03-24 13:29:07 +0100

    CommandUtils: implement couple of useful function

--- src/main/java/pl/lifelesspixels/lputilities/commands/CommandUtils.java
+++ src/main/java/pl/lifelesspixels/lputilities/commands/CommandUtils.java
@@ -9,4 +9,16 @@ public class CommandUtils {
         sender.sendMessage(ChatColor.RED + "You have no permissions to run /" + commandName + " command");
     }
 
+    public static void sendUsage(CommandSender sender, String alias, String argumentsString) {
+        sender.sendMessage(ChatColor.RED + "Usage: " + ChatColor.RESET + "/" + alias + " " + argumentsString);
+    }
+
+    public static void sendPlayerNotFound(CommandSender sender, String playerName) {
+        sender.sendMessage(ChatColor.RED + "Player " + ChatColor.RESET + playerName + ChatColor.RED + " cannot be found");
+    }
+
+    public static void sendCannotParseInteger(CommandSender sender, String integerString) {
+        sender.sendMessage(ChatColor.RED + "Integer " + ChatColor.RESET + integerString + ChatColor.RED + " is invalid");
+    }
+
 }

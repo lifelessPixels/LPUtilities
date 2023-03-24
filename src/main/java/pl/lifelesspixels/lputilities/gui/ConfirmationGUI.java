7848ec6aa69c38b5a7c912e260ea356fcf467c68
Karol Małecki <malecki.karol@outlook.com>
2023-03-24 22:15:32 +0100

    LPUtilities: implement simple confirmation GUI

---
+++ src/main/java/pl/lifelesspixels/lputilities/gui/ConfirmationGUI.java
@@ -0,0 +1,83 @@
+package pl.lifelesspixels.lputilities.gui;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import pl.lifelesspixels.lpchestgui.data.ChestGUIClickActionBuilder;
+import pl.lifelesspixels.lpchestgui.data.ChestGUIClickActionResult;
+import pl.lifelesspixels.lpchestgui.gui.ChestGUI;
+import pl.lifelesspixels.lputilities.heads.CustomHeads;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.Callable;
+
+public class ConfirmationGUI extends ChestGUI {
+
+    private static final ItemStack QUESTION_MARK_HEAD = CustomHeads.createHeadFromBase64(
+            "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYmM4ZWExZjUxZjI1M2ZmNTE0MmNhMTFhZTQ1MTkzYTRhZDhjM2FiNWU5YzZlZWM4YmE3YTRmY2I3YmFjNDAifX19");
+
+    private static final ItemStack TICK_HEAD = CustomHeads.createHeadFromBase64(
+            "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOGE5OTM0MmUyYzczYTlmMzgyMjYyOGU3OTY0ODgyMzRmMjU4NDQ2ZjVhMmQ0ZDU5ZGRlNGFhODdkYjk4In19fQ==");
+
+    private static final ItemStack CROSS_HEAD = CustomHeads.createHeadFromBase64(
+            "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMTZjNjBkYTQxNGJmMDM3MTU5YzhiZThkMDlhOGVjYjkxOWJmODlhMWEyMTUwMWI1YjJlYTc1OTYzOTE4YjdiIn19fQ==");
+
+    private Runnable cancelCallback;
+
+    public ConfirmationGUI(String promptText, List<String> extendedPrompt, Runnable confirmCallback, Runnable cancelCallback) {
+        super(3, "Confirmation");
+        this.cancelCallback = cancelCallback;
+
+        // set up the UI
+        setDummyItem(13, createPromptItem(promptText, extendedPrompt));
+        setAction(11, new ChestGUIClickActionBuilder().withLeftClickCallback(context -> {
+            if(confirmCallback != null)
+                confirmCallback.run();
+            return ChestGUIClickActionResult.Close;
+        }).build(), createConfirmItem(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP);
+        setAction(15, new ChestGUIClickActionBuilder().withLeftClickCallback(context -> {
+            if(cancelCallback != null)
+                cancelCallback.run();
+            return ChestGUIClickActionResult.Close;
+        }).build(), createCancelItem(), Sound.ENTITY_ITEM_BREAK);
+    }
+
+    @Override
+    public void onInventoryClosed(Player player) {
+        if(cancelCallback != null)
+            cancelCallback.run();
+        player.playSound(player, Sound.ENTITY_ITEM_BREAK, 1.0f, 1.0f);
+    }
+
+    private ItemStack createPromptItem(String promptText, List<String> extendedPrompt) {
+        ItemStack promptItem = QUESTION_MARK_HEAD.clone();
+        ItemMeta meta = Objects.requireNonNull(promptItem.getItemMeta());
+        meta.setDisplayName(promptText);
+        if(extendedPrompt != null) {
+            meta.setLore(extendedPrompt);
+        }
+
+        promptItem.setItemMeta(meta);
+        return promptItem;
+    }
+
+    private ItemStack createConfirmItem() {
+        ItemStack confirmItem = TICK_HEAD.clone();
+        ItemMeta meta = Objects.requireNonNull(confirmItem.getItemMeta());
+        meta.setDisplayName(ChatColor.GREEN + "Confirm");
+        confirmItem.setItemMeta(meta);
+        return confirmItem;
+    }
+
+    private ItemStack createCancelItem() {
+        ItemStack cancelItem = CROSS_HEAD.clone();
+        ItemMeta meta = Objects.requireNonNull(cancelItem.getItemMeta());
+        meta.setDisplayName(ChatColor.RED + "Cancel");
+        cancelItem.setItemMeta(meta);
+        return cancelItem;
+    }
+
+}
